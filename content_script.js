a4382d1cc816232815abf66eed1a3ba04e114e65
Abraao Lourenco <abraao@guarancode.com>
2011-08-04 19:22:09 +0200

    Created project. At this point the extension loads both the content script and jquery correctly.

---
+++ content_script.js
@@ -0,0 +1,13 @@
+var reviewbuddy = {};
+
+/**
+ * Inject our code into the page's DOM.
+ */
+reviewbuddy.injectIntoPage = function() {
+	alert("doctor!");
+	
+}
+
+reviewbuddy.createReview = function() {
+	alert("create review");
+}
