1adb20c30875317db7ab2be3e355dd0a76947f54
Abraao Lourenco <abraao@guarancode.com>
2011-08-10 23:03:16 +0200

    Created hello world option page.

---
+++ options/options.js
@@ -0,0 +1,8 @@
+var reviewbuddy = {};
+var reviewbuddy.options = {};
+
+reviewbuddy.options.pageSetup = function() {
+	$(document).ready(function() { alert("hello world"); });
+}
+
+reviewbuddy.options.pageSetup();
